0a4adfe1697810e167ef333204017231d3aff6d4
M-Ridho-Fauzan <ridho.213040036@mail.unpas.ac.id>
2024-01-07 05:25:56 +0700

    Tubes PP2 85%

--- src/main/java/db/e-waste.sql
+++ src/main/java/db/e-waste.sql
@@ -19,6 +19,17 @@
 CREATE DATABASE IF NOT EXISTS `e-waste` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `e-waste`;
 
+-- Dumping structure for table e-waste.kategori_terbanyak
+CREATE TABLE IF NOT EXISTS `kategori_terbanyak` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `nama_kategori` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `jumlah_order` int DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `kategori_nama_unique` (`nama_kategori`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Dumping data for table e-waste.kategori_terbanyak: ~0 rows (approximately)
+
 -- Dumping structure for table e-waste.kurir
 CREATE TABLE IF NOT EXISTS `kurir` (
   `id` bigint unsigned NOT NULL AUTO_INCREMENT,
@@ -31,23 +42,24 @@ CREATE TABLE IF NOT EXISTS `kurir` (
 
 -- Dumping data for table e-waste.kurir: ~0 rows (approximately)
 
--- Dumping structure for table e-waste.sampah
-CREATE TABLE IF NOT EXISTS `sampah` (
+-- Dumping structure for table e-waste.orders
+CREATE TABLE IF NOT EXISTS `orders` (
   `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `name_sampah` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `kategori_sampah` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `alamat_user` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `email_user` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `daerah` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `total_sampah` int DEFAULT '0',
+  `kategori_sampah` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `email_kurir` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `view_sampah` int DEFAULT '0',
+  `tanggal_order` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `email_kurir` (`email_kurir`),
   KEY `email_user` (`email_user`),
-  CONSTRAINT `sampah_ibfk_1` FOREIGN KEY (`email_kurir`) REFERENCES `kurir` (`email_kurir`),
-  CONSTRAINT `sampah_ibfk_2` FOREIGN KEY (`email_user`) REFERENCES `users` (`email_user`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  KEY `total_sampah` (`total_sampah`,`email_user`,`daerah`,`email_kurir`),
+  CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`email_kurir`) REFERENCES `kurir` (`email_kurir`),
+  CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`email_user`) REFERENCES `users` (`email_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Dumping data for table e-waste.sampah: ~0 rows (approximately)
+-- Dumping data for table e-waste.orders: ~0 rows (approximately)
 
 -- Dumping structure for table e-waste.users
 CREATE TABLE IF NOT EXISTS `users` (
@@ -62,12 +74,11 @@ CREATE TABLE IF NOT EXISTS `users` (
   `is_admin` tinyblob NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `users_email_unique` (`email_user`)
-) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Dumping data for table e-waste.users: ~2 rows (approximately)
+-- Dumping data for table e-waste.users: ~1 rows (approximately)
 REPLACE INTO `users` (`id`, `name`, `email_user`, `password`, `telp`, `alamat`, `point`, `is_kurir`, `is_admin`) VALUES
-	(13, 'admin', 'admin@gmail.com', 'admin123', '081234567890', 'Bandung', 0, _binary 0x30, _binary 0x31),
-	(14, 'nama saya ini', 'ini@gmail.com', '123123', '56789098765', 'alamat saya ini', 0, _binary 0x30, _binary 0x30);
+	(11, 'admin', 'admin@gmail.com', 'admin123', '081234567890', 'Bandung', 0, _binary 0x30, _binary 0x31);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
